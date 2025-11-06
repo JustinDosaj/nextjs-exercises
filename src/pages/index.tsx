c07aff2e337222aeb5875c07478d9aa47087e6cf
JustinDosaj <justindosaj@gmail.com>
2025-11-06 13:04:10 -0800

    Remove Test Code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,36 +12,9 @@ import { ListGenerationAndFilter } from "@/excercises/ListGenerationAndFilter";
 import { AlbumSuspense } from "@/excercises/AlbumSuspense";
 
 
-/**
- * Uncomment which solution you would like to see
- */
-
 export default function Home() {
 
   const [checked, setChecked] = useState<boolean>(false);
-
-  // Random Obj + Map Stuff //
-  const test = new Map<string, number>();
-  test.set("apple", 5)
-  test.set("banana", 10)
-
-  console.log("Test: ", test.values())
-
-  for (const [key, value] of test) {
-      console.log(`${key}: ${value}`)
-  }
-
-  const obj: Record<string, number> = {}
-
-  obj.name = 5
-
-  console.log("Object: ", obj)
-  console.log("Object Keys: ", Object.keys(obj))
-  console.log("Object Values: ", Object.values(obj))
-
-  Object.keys(obj).forEach((key) => console.log("For Each Key: ", key))
-
-  // End Random Obj + Map Stuff //
   
   return (
       <Layout>
